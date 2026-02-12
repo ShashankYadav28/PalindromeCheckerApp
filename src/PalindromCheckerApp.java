30ec92b77e82b6dbce079d1b268d9f33c07d5608
ShashankYadav28 <shashankyadav2803@gmail.com>
2026-02-12 10:25:42 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromCheckerApp {
+    public static void main(String args[]) {
+    System.out.println("jai shree ram,\n" +
+            "Welcome to the Palindrome Checker App\nSystem Intialised Successfully");
+
+    }
+}
