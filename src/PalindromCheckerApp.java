f0ac1ce6882f3d89966f35539a95b420f3064d1e
ShashankYadav28 <shashankyadav2803@gmail.com>
2026-02-19 10:30:55 +0530

    palindrome checking app

--- src/PalindromCheckerApp.java
+++
@@ -1,7 +0,0 @@
-public class PalindromCheckerApp {
-    public static void main(String args[]) {
-    System.out.println("jai shree ram,\n" +
-            "Welcome to the Palindrome Checker App\nSystem Intialised Successfully");
-
-    }
-}
---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,4 @@
+public class PalindromCheckerApp {
+    public static void main(String args[]) {
+    
+}
